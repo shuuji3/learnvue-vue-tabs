69b147768970e0bb093de8f7c8c86801202adcf5
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-05-01 21:14:49 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LearnVue vue-tabs
+
+[Building a Reusable Tabs Component with Vue Slots - YouTube](https://www.youtube.com/watch?v=ROGeJxI2L58)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Tab from "./components/Tab.vue";
+import TabsWrapper from "./components/TabsWrapper.vue";
+</script>
+
+<template>
+  <TabsWrapper>
+    <Tab title="Tab 1">Hello from Tab 1</Tab>
+    <Tab title="Tab 2">Hello from Tab 2</Tab>
+    <Tab title="Tab 3">Hello from Tab 3</Tab>
+    <Tab title="Tab 4">Hello from Tab 4</Tab>
+  </TabsWrapper>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
